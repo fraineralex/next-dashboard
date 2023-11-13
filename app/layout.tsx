1d318acc6eee179bdc6c7ee81f400a94891b78ad
Frainer Encarnación <fraineralex2001@gmail.com>
2023-11-13 16:31:08 -0400

    fix: move the favicon. ico and opengraph-image.png to the /app dir

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
     default: 'Acme Dashboard',
   },
   description: 'The official Next.js Learn Dashboard built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  metadataBase: new URL('https://frainer-next-dashboard.vercel.app/'),
 };
 
 export default function RootLayout ({
