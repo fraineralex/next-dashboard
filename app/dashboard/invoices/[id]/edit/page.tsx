ff3d53da2c68f50cc92a3e005847c2ddd424ce6e
Frainer Encarnación <fraineralex2001@gmail.com>
2023-11-13 16:04:43 -0400

    feat: add the corresponding metadata to the app and complete the chapter 16 'Adding metadata' of the nextjs course

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,6 +3,11 @@ import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data'
 import { InvoiceForm } from '@/app/lib/definitions'
 import { notFound } from 'next/navigation'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Edit Invoice'
+}
 
 export default async function Page ({ params }: { params: { id: string } }) {
   const id = params.id
